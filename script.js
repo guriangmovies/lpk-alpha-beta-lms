ede8faf92f2a72a77c2091d6de7dfdc2314f2759
guriangmovies <72215991+guriangmovies@users.noreply.github.com>
2026-02-21 11:28:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw2JEo7wXPocpUf2bYFzcPRI5FZPKafnqhNeV5Uf0HpDsWpUczbhllEW54oDG26Xa-S/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbybhwLikQLQ_DFo3lqFgSn7pzsxdXBzrAMjGOvcqvo3RAZz_96X0cmHJpIrrdm5mbM/exec";
 
 // =====================================
 // 1. FITUR UI MODERN (LOADER & TOAST)
